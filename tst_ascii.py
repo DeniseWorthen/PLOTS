0d7f077e46d79674065482038a330865eae46ce7
Denise Worthen <denise.worthen@noaa.gov>
2025-10-15 15:54:12 -0400

    clean up toy ascii script

--- tst_ascii.py
+++ tst_ascii.py
@@ -3,23 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load dataset
-
-#dirsrc="/gpfs/f6/infra-cpu/world-shared/Denise.Worthen/freerun2021/"
 dirsrc="/scratch3/NCEPDEV/stmp/Denise.Worthen/residual.ice/"
 
 data = np.loadtxt(dirsrc+"heaterror.sfs.dev.dat")
 #print(data.shape[0])
 
-
+# known start date and frequency of data
 start_date = '2021-05-01 06:00:00'
-#end_date = '2021-06-16 00:00:00'
-
 time_axis = xr.date_range(start=start_date, periods=len(data), freq='6h')
 #print(time_axis)
 
-#print(data)
-#hen="arwt heat error"
-
+# create a dataset from the two data columns and the time axis
 ds = xr.Dataset(
     {"hen": (("time"), data[:, 0]),
      "hes": (("time"), data[:, 1])},
@@ -29,22 +23,16 @@ ds = xr.Dataset(
 var1=ds["hen"]
 var2=ds["hes"]
 tvals=ds["time"]
-
-
 #print(ds)
+
+# plot the data vs time and label the lines
 plt.figure(figsize=(10, 6))
 plt.plot(tvals,var1,label="arwt heat error: Arctic")
 plt.plot(tvals,var2,label="arwt heat error: Antarctic")
-
-#plt.title(ds1title)
-#plt.ylabel(var1.attrs.get("units", ""))
 
 plt.xlabel("Time")
 plt.grid(True)
 plt.legend()
 
 plt.tight_layout()
-#ds.plot()
-#plt.show()
-#ds.plot()
 plt.show()
