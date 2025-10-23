aef353403ecff4c974f3c672467005b733064890
Denise Worthen <denise.worthen@noaa.gov>
2025-10-23 10:47:01 -0400

    legending

--- cmeps.tst.py
+++ cmeps.tst.py
@@ -49,14 +49,22 @@ data3 = np.loadtxt(dirsrc+"tst4/post_ocn.dat")
 data4 = np.loadtxt(dirsrc+"tst4/post_wav.dat")
 tst4dat = np.stack([data0,data1,data2,data3,data4],axis=0)
 
+data0 = np.loadtxt(dirsrc+"tst5/rhinit.dat")
+data1 = np.loadtxt(dirsrc+"tst5/post_atm.dat")
+data2 = np.loadtxt(dirsrc+"tst5/post_ice.dat")
+data3 = np.loadtxt(dirsrc+"tst5/post_ocn.dat")
+data4 = np.loadtxt(dirsrc+"tst5/post_wav.dat")
+tst5dat = np.stack([data0,data1,data2,data3,data4],axis=0)
+
 # Panels same figure
-factors=[1.0,1.0,1.0,1.0,1.0,1.0]
+factors=[1.0,1.0,1.0,1.0,1.0]
 #factors = [1.0, aifac, aifac, owfac, owfac]
 print(factors)
 titles = ['RHinit','post_atm','post_ice','post_ocn','post_wav']
 nrow=len(titles)
 ncol=1
 fig, axs = plt.subplots(nrow, ncol, figsize=(8,10), sharex=True)
+varname="mean"
 
 for ii, title in enumerate(titles):
     pecnt=tst1dat[0,:,0]
@@ -87,18 +95,50 @@ for ii, title in enumerate(titles):
         coords={"pecnt": pecnt}
     )
 
+    ds5 = xr.Dataset(
+        {"mean": (("pecnt"), factors[ii]*tst5dat[ii,:, 1]),
+         "min": (("pecnt"),  factors[ii]*tst5dat[ii,:, 2]),
+         "max": (("pecnt"),  factors[ii]*tst5dat[ii,:, 3])},
+        coords={"pecnt": pecnt}
+    )
+
+
     axs[ii].set_title(title)
-    axs[ii].plot(pecnt, ds1["mean"],marker='o', label="base")
-    #axs[ii].plot(pecnt, ds2["mean"],marker='o', label="mapfiles")
-    axs[ii].plot(pecnt, ds3["mean"],marker='o', label="cmeps rhs")
-    axs[ii].plot(pecnt, ds4["mean"],marker='o', label="cmeps rhs (rpt)")
+    if (ii < nrow-1):
+        axs[ii].plot(pecnt, ds1[varname],marker='o', color='blue')
+        axs[ii].plot(pecnt, ds5[varname],marker='x', color='blue')
+        axs[ii].plot(pecnt, ds2[varname],marker='o', color='cyan')
+        axs[ii].plot(pecnt, ds3[varname],marker='o', color='green')
+        axs[ii].plot(pecnt, ds4[varname],marker='x', color='green')
+    else:
+        axs[ii].plot(pecnt, ds1[varname],marker='o', color='blue', label="base")
+        axs[ii].plot(pecnt, ds5[varname],marker='x', color='blue', label="base (rpt)")
+        axs[ii].plot(pecnt, ds2[varname],marker='o', color='cyan', label="mapfiles")
+        axs[ii].plot(pecnt, ds3[varname],marker='o', color='green', label="cmeps rhs")
+        axs[ii].plot(pecnt, ds4[varname],marker='x', color='green', label="cmeps rhs (rpt)")
+    #endif
+    #vmin=0
+    #vmax=20
+    #axs[ii].set_ylim(vmin,vmax)
 
     axs[nrow-1].set_xlabel("PE count")
 
 # Add grid and legends to subplots
 for ax in axs:
     ax.grid(True)
-    ax.legend()
+    #ax.legend()
+
+# Collect handles and labels from all subplots
+handles, labels = [], []
+for ax in axs:
+    h, l = ax.get_legend_handles_labels()
+    handles.extend(h)
+    labels.extend(l)
+
+# Place the legend outside the panel
+fig.legend(handles, labels, loc='lower right', bbox_to_anchor=(.95, 0.65))
+fig.tight_layout(rect=[0, .15, 1, 1]) # Adjust rect to leave space on the right)
 
 plt.tight_layout()
 plt.show()
+exit
